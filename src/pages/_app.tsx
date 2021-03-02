cbdb12ec7f2b52ab930dfad9cbb64b93b5273dd5
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2021-03-02 19:10:03 -0300

    init project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,7 @@
-export default function MyApp({ Component, pageProps }) {
+import '../styles/global.css'
+
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
+import { CompletedChallenges } from '../components/CompletedChallenges'
+import { ExperienceBar } from '../components/ExperienceBar'
+import { Profile } from '../components/Profile'
+import { Countdown } from '../components/Countdown'
+import Head from 'next/head'
+
+import styles from '../styles/pages/Home.module.css'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className={styles.container}>
+      <Head>
+        <title>Inicio | Moveit</title>
+      </Head>
+      
+      <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div></div>
+      </section>
     </div>
   )
 }
