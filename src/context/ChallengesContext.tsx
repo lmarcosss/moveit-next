2d37bc77e86b2ece874c88a8e3bf20865b219a97
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2021-03-05 14:05:14 -0300

    create challenge context

---
+++ src/context/ChallengesContext.tsx
@@ -0,0 +1,88 @@
+import { createContext, useState, ReactNode } from 'react'
+import challenges from '../../challenges.json'
+
+enum ChallengeType {
+    BODY = 'body',
+    EYE = 'eye',
+}
+
+export interface Challenge {
+    type: ChallengeType;
+    description: string;
+    amount: number;
+}
+
+interface ChallengesContextData {
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
+    levelUp: () => void;
+    startNewChallenge: () => void;
+    activeChallenge: Challenge;
+    resetChallenge: () => void;
+    experienceToNextLevel: number;
+    completeChallenge: () => void;
+}
+
+interface ChallengesProviderProps {
+    children: ReactNode; 
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData)
+
+export function ChallengesProvider({ children }: ChallengesProviderProps) {
+    const [level, setLevel] = useState(1)
+    const [currentExperience, setCurrentExperience] = useState(0)
+    const [challengesCompleted, setChallengesCompleted] = useState(0)
+    const [activeChallenge, setActiveChallenge] = useState(null)
+
+    const experienceToNextLevel = Math.pow(((level + 1) * 4), 2)
+
+    function levelUp() {
+        setLevel(level + 1)
+    }
+
+    function startNewChallenge() {
+        const radomChallengeIndex = Math.floor(Math.random() * challenges.length)
+        const challenge = challenges[radomChallengeIndex]
+
+        setActiveChallenge(challenge)
+    }
+
+    function resetChallenge() {
+        setActiveChallenge(null)
+    }
+
+    function completeChallenge() {
+        if (!activeChallenge) return
+
+        const { amount } = activeChallenge
+
+        let finalExperience = currentExperience + amount
+
+        if (finalExperience >= experienceToNextLevel) {
+            finalExperience = finalExperience - experienceToNextLevel
+            levelUp()
+        }
+
+        setCurrentExperience(finalExperience)
+        setActiveChallenge(null)
+        setChallengesCompleted(challengesCompleted + 1)
+    }
+
+    return (
+        <ChallengesContext.Provider value={{
+            level,
+            currentExperience,
+            challengesCompleted,
+            levelUp,
+            startNewChallenge,
+            activeChallenge,
+            resetChallenge,
+            experienceToNextLevel,
+            completeChallenge,
+        }}>
+            {children}
+        </ChallengesContext.Provider>
+    )
+}
