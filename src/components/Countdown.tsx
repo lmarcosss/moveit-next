e3b181af9adae8df0816f7c5c3990403e5c5f514
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2021-03-11 02:20:31 -0300

    adjust countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 import { CountdownContext } from '../context/CountdowContext'
-import styles from '../styles/components/CountDown.module.css'
+import styles from '../styles/components/Countdown.module.css'
 
 interface IButton {
     isActive: boolean;
