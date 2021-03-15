9664c76c0ed822748b0a51d650f0e07f4a768afb
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2021-03-15 00:14:27 -0300

    adjust Modal

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,4 +1,11 @@
 import styles from '../../styles/components/common/Modal.module.css'
+import { ReactNode } from 'react'
+
+interface Props {
+    closeModal: () => void;
+    children: ReactNode;
+    containerStyles?: string;
+}
 export function Modal({ closeModal, children, containerStyles }) {
     return (
         <div className={styles.overlay}>
